bcf0bc083bd6d2b40a9828da7f2fc3e4b0725007
Aliaksander <skripel@pm.me>
2023-04-18 22:03:31 +0300

    закончил урок

--- homeworks/l9/01_char_stat.py
+++ homeworks/l9/01_char_stat.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from tabulate import tabulate
+from abc import ABC, abstractmethod
 
 
-class Charstat:
+class Charstat(ABC):
     char_stats = {}
 
     def __init__(self):
@@ -22,6 +23,16 @@ class Charstat:
                         else:
                             self.char_stats[char] = 0
 
+    @abstractmethod
+    def sort_dict(self):
+        pass
+
+    @abstractmethod
+    def start_sort(self):
+        pass
+
+
+class Sort1(Charstat):
     def sort_dict(self):
         result = sorted(self.char_stats.items(), key=lambda x: x[1], reverse=True)
         total = sum(self.char_stats.values())
@@ -29,13 +40,54 @@ class Charstat:
         result.append(('Итого', total))
         return result
 
+    def start_sort(self):
+        print(tabulate(chars.sort_dict(), headers=['Буква', 'Частота'], tablefmt="psql", stralign='center',
+                       numalign='center'))
 
-chars = Charstat()
+
+class Sort2(Charstat):
+    def sort_dict(self):
+        result = sorted(self.char_stats.items(), key=lambda x: x[1], reverse=False)
+        total = sum(self.char_stats.values())
+        result.append('')
+        result.append(('Итого', total))
+        return result
+
+    def start_sort(self):
+        print(tabulate(chars.sort_dict(), headers=['Буква', 'Частота'], tablefmt="psql", stralign='center',
+                       numalign='center'))
+
+
+class Sort3(Charstat):
+    def sort_dict(self):
+        result = sorted(self.char_stats.items(), key=lambda x: x[0], reverse=False)
+        total = sum(self.char_stats.values())
+        result.append('')
+        result.append(('Итого', total))
+        return result
+
+    def start_sort(self):
+        print(tabulate(chars.sort_dict(), headers=['Буква', 'Частота'], tablefmt="psql", stralign='center',
+                       numalign='center'))
+
+
+class Sort4(Charstat):
+    def sort_dict(self):
+        result = sorted(self.char_stats.items(), key=lambda x: x[0], reverse=True)
+        total = sum(self.char_stats.values())
+        result.append('')
+        result.append(('Итого', total))
+        return result
+
+    def start_sort(self):
+        print(tabulate(chars.sort_dict(), headers=['Буква', 'Частота'], tablefmt="psql", stralign='center',
+                       numalign='center'))
+
+
+chars = Sort3()
 chars.get_file("Война и мир.txt")
 chars.open_file()
-
-print(tabulate(chars.sort_dict(), headers=['Буква', 'Частота'], tablefmt="psql", stralign='center', numalign='center'))
-
+chars.start_sort()
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
 #  - по алфавиту по возрастанию
