c0e24a24dc3ca3296937eae91b1e080fb59ac1c2
Aliaksander <skripel@pm.me>
2023-04-18 21:38:48 +0300

    сделал основу

--- homeworks/l9/01_char_stat.py
+++ homeworks/l9/01_char_stat.py
@@ -1,28 +1,9 @@
 # -*- coding: utf-8 -*-
-from pprint import pprint
-from tabulate import tabulate
-# Подсчитать статистику по буквам в романе Война и Мир.
-# Входные параметры: файл для сканирования
-# Статистику считать только для букв алфавита (см функцию .isalpha() для строк)
-#
-# Вывести на консоль упорядоченную статистику в виде
-# +---------+----------+
-# |  буква  | частота  |
-# +---------+----------+
-# |    А    |   77777  |
-# |    Б    |   55555  |
-# |   ...   |   .....  |
-# |    a    |   33333  |
-# |    б    |   11111  |
-# |   ...   |   .....  |
-# +---------+----------+
-# |  итого  | 9999999  |
-# +---------+----------+
-#
-# Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
-# Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-class Charstats:
+from tabulate import tabulate
+
+
+class Charstat:
     char_stats = {}
 
     def __init__(self):
@@ -42,17 +23,18 @@ class Charstats:
                             self.char_stats[char] = 0
 
     def sort_dict(self):
-        total = sum(self.char_stats.values())
         result = sorted(self.char_stats.items(), key=lambda x: x[1], reverse=True)
+        total = sum(self.char_stats.values())
+        result.append('')
         result.append(('Итого', total))
         return result
 
 
-chars = Charstats()
+chars = Charstat()
 chars.get_file("Война и мир.txt")
 chars.open_file()
 
-print(tabulate(chars.sort_dict(), headers=['Буква', 'Частота'], tablefmt="psql", stralign='center'))
+print(tabulate(chars.sort_dict(), headers=['Буква', 'Частота'], tablefmt="psql", stralign='center', numalign='center'))
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
