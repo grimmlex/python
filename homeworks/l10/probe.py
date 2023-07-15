9ae33e56bc783045aa72ef1b720d2860a3b4221e
Skripel_notebook <s.skripel@gmail.com>
2023-07-15 18:48:36 +0300

    Закочил дз 11 урока

--- homeworks/l10/probe.py
+++ homeworks/l10/probe.py
@@ -1,27 +1,20 @@
-import time
-def timer(f):
-    def tmp(*args, **kwargs):
-        t = time.time()
-        res = f(*args, **kwargs)
-        print("Время выполнения функции: %f" % (time.time()-t))
-        return res
-
-    return tmp
-
-def pause(f):
-    def tmp(*args, **kwargs):
-        time.sleep(1)
-        return f(*args, **kwargs)
-
-    return tmp
-
-@timer
+def benchmark(func):
+    import time
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f'Время выполнения {end-start}')
+        return result
+    return wrapper
 
 
-def func(x, y):
-    return x + y * x -x**y
 
-print(func(103, 1231))
-print(func(10, 1231))
-print(func(103, 121))
-print(func(10, 31))
+@benchmark
+def dtf(url):
+    import requests
+    webpage = requests.get(url)
+    return webpage.text
+
+result = dtf('https://dtf.ru/')
+print(result)
--- homeworks/l11/04_error_log_decorator.py
+++ homeworks/l11/04_error_log_decorator.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import logging
 # Написать декоратор, который будет логировать (записывать в лог файл)
 # ошибки из декорируемой функции и выбрасывать их дальше.
 #
@@ -8,10 +8,28 @@
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
 
 
-def log_errors(func):
-    pass
-    # TODO здесь ваш код
 
+def log_errors(file_name):
+    def actual_decorator(func):
+        def wrapper(*args, **kwargs):
+            import logging
+
+            root_logger = logging.getLogger()
+            root_logger.setLevel(logging.WARNING)
+            handler = logging.FileHandler(file_name, 'a', 'utf-8')
+            handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+            root_logger.addHandler(handler)
+
+            print('Начинаем')
+            try:
+                func(*args, **kwargs)
+            except:
+                logging.warning('WArnsk', exc_info=True)
+                print('Записали ошибку')
+            return
+
+        return wrapper
+    return actual_decorator
 
 # Проверить работу на следующих функциях
 @log_errors
@@ -38,18 +56,20 @@ lines = [
     'Земфира 86',
     'Равшан wmsuuzsxi@mail.ru 35',
 ]
-for line in lines:
-    try:
-        check_line(line)
-    except Exception as exc:
-        print(f'Invalid format: {exc}')
-perky(param=42)
-
+# for line in lines:
+#     try:
+#         check_line(line)
+#     except Exception as exc:
+#         print(f'Invalid format: {exc}')
+# perky(param=440)
+#
 
 # Усложненное задание (делать по желанию).
 # Написать декоратор с параметром - именем файла
 #
-# @log_errors('function_errors.log')
-# def func():
-#     pass
+@log_errors('function_errors.log')
+def func(i):
+    return i / 0
 
+
+func(10)
