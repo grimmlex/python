615fa10893a390f179cf47e8d2eceb966aa2df96
skripel <s.skipel@gmail.com>
2023-11-05 20:34:46 +0300

    13414112

--- homeworks/l4/probe.py
+++ homeworks/l4/probe.py
@@ -5,3 +5,30 @@ def domain_name(url):
 url = "http://www.google.com"
 
 print(domain_name(url))
+
+
+
+input_str = [2, 4, 2, 6, 2, 8]
+
+import math
+from fractions import Fraction
+
+def convert_fracts(lst):
+    denominators = []
+    numerators = []
+    fracts = {}
+    if isinstance(lst, str):
+        lst = lst.split(',')
+
+    for i in range(0, len(lst)):
+        if i % 2 != 0:
+            denominators.append(lst[i])
+        else:
+            numerators.append(lst[i])
+
+    fracts = dict(zip(numerators, denominators))
+    print(fracts)
+
+
+print(math.gcd(2,3,4))
+convert_fracts(input_str)
