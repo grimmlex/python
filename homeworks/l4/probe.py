27114ece29cd50c47fc9b057f7892f3462e0b62b
Skripel <skripel@pm.me>
2023-11-05 20:36:35 +0300

    настроил на ноуте

--- homeworks/l4/probe.py
+++ homeworks/l4/probe.py
@@ -1,13 +1,3 @@
-def domain_name(url):
-    result = url.split('//')[-1].split('www.')[-1].split('.')[0]
-    return result
-
-url = "http://www.google.com"
-
-print(domain_name(url))
-
-
-
 input_str = [2, 4, 2, 6, 2, 8]
 
 import math
