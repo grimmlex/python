ee72db46860efe6d03d873141d99724abfbef75c
skripel <s.skipel@gmail.com>
2023-11-26 23:25:12 +0300

    закончил на сегодня

--- homeworks/l4/probe.py
+++ homeworks/l4/probe.py
@@ -16,31 +16,23 @@ def duplicate_encode(word):
 
 # // ]]]21.11.2023
 
-prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61]
+from collections import defaultdict
+
+def dec(n):
+    decompres = defaultdict(lambda: 0)
+    i = 2
+    while n > 1:
+        while n % i == 0:
+            n /= i
+            decompres[i] += 1
+        i += 1
+    return decompres
+
 def decomp(n):
-    arr = {}
-    fact_number = factorial(n)
-    if factorial(n) == 1:
-        return 1
-    for num in prime_numbers:
-        decomp_division(fact_number, num)
+    answer = defaultdict(lambda: 0)
+    for i in range(2, n + 1):
+        for key, value in dec(i).items():
+            answer[key] += value
+    return ' * '.join(f'{x}^{y}' if y > 1 else str(x) for x, y in sorted(answer.items()))
 
-
-
-def decomp_division(n, num):
-    i = 1
-    while True:
-        number = n / num
-        print(number)
-        if number % num != 0:
-            print('test')
-            break
-            return False
-        else:
-            i += 1
-            continue
-    return i
-
-decomp(8)
-
-print(5 % 2 != 0)
+print(decomp(6))
